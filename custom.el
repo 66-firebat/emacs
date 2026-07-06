d2c7de39adec6f615c7977cd260ffff6ad8da451
firebat <66.firebat@gmail.com>
2026-07-06 18:28:31 +0000

    |.git| = 4

---
+++ custom.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(avy diff-hl doom-modeline eat embark-consult evil-collection general
+	 kkp magit marginalia orderless pi-coding-agent vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ elpa/pi-coding-agent-20260702.1052/pi-coding-agent-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pi-coding-agent" "20260702.1052"
+  "Emacs frontend for pi coding agent."
+  '((emacs               "29.1")
+    (transient           "0.9.0")
+    (md-ts-mode          "0.3.0")
+    (markdown-table-wrap "0.2.0"))
+  :url "https://github.com/dnouri/pi-coding-agent"
+  :commit "2b6a27feb6d224aeb9b680941a925c7c42948c59"
+  :revdesc "2b6a27feb6d2"
+  :keywords '("ai" "llm" "ai-pair-programming" "tools")
+  :authors '(("Daniel Nouri" . "daniel.nouri@gmail.com"))
+  :maintainers '(("Daniel Nouri" . "daniel.nouri@gmail.com")))
