31ca859db2bf7e19bb756094f4a42dae89d5297f
fireshark <66.firebat@gmail.com>
2026-07-14 11:47:06 -0600

    |.git| = 13884

--- diff-hl.el
+++ diff-hl.el
@@ -16,8 +16,8 @@
   :config
   ;; Left margin display with Nerd Font icons
   (setq diff-hl-margin-symbols-alist
-        '((insert . " 󰐗 ") (delete . " 󰅙 ") (change . " 󰆗 ")
-          (unknown . "  ") (ignored . " 󰍶 ") (reference . " 󱆮 ")))
+        '((insert . "  ") (delete . "  ") (change . " 󰑧 ")
+          (unknown . " 󱔢 ") (ignored . "  ") (reference . " 󰔰 ")))
 
   ;; Enable globally in all file-visiting buffers
   (global-diff-hl-mode 1)
